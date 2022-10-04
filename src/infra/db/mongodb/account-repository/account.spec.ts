f38cd6fa0639da78f23ab04b123547610130cbc6
Victor Alencar <munternl13@gmail.com>
2022-10-04 15:50:08 -0300

    test: ensure LogMongoRepository creates an error log on success

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -11,7 +11,7 @@ describe('Account Mongo Repository', () => {
   })
 
   beforeEach(async () => {
-    const accountCollection = MongoHelper.getColletion('accounts')
+    const accountCollection = MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
 
