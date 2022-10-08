b4cf730708996d835ea91dcfb2edc5d5157207c5
Victor Alencar <munternl13@gmail.com>
2022-10-08 18:44:23 -0300

    refactor: change Authentication to receive a model

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,11 +1,11 @@
-import { Authentication, HttpRequest, Validation } from './login-protocols'
+import { Authentication, HttpRequest, Validation, AuthenticationModel } from './login-protocols'
 import { LoginController } from './login'
 import { badRequest, ok, serverError, unauthorized } from '../../helpers/http/http-helper'
 import { MissingParamError } from '../../errors'
 
 const makeAuthentication = (): Authentication => {
   class AuthenticationStub implements Authentication {
-    async auth (email: string, password: string): Promise<string> {
+    async auth (authentication: AuthenticationModel): Promise<string> {
       return new Promise(resolve => resolve('any_token'))
     }
   }
@@ -50,7 +50,10 @@ describe('Login Controller', () => {
     const { sut, authenticationStub } = makeSut()
     const authSpy = jest.spyOn(authenticationStub, 'auth')
     await sut.handle(makeFakeRequest())
-    expect(authSpy).toHaveBeenCalledWith('any_mail@email.com', 'any_password')
+    expect(authSpy).toHaveBeenCalledWith({
+      email: 'any_mail@email.com',
+      password: 'any_password'
+    })
   })
 
   it('Should return 401 if invalid credentials are provided', async () => {
