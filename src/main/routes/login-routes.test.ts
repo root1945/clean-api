aefcd33e0055d7fe7ace7bbb1285f6c8f4e16d08
Victor Alencar <munternl13@gmail.com>
2022-10-14 00:01:35 -0300

    test: login route returns 401 if invalid credentials

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -50,5 +50,15 @@ describe('Login Routes', () => {
         })
         .expect(200)
     })
+
+    it('Should return 401 on login', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'any_email@mail.com',
+          password: 'victor123'
+        })
+        .expect(401)
+    })
   })
 })
